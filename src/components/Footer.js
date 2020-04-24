ba38f2133ba3579b5561e784006e6d2ba3897fa3
diego.pereira <diego.pereira@farfetch.com>
2020-04-24 11:48:40 +0100

    fix: url git

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer className="m-3">
-            Developed by <a href="https://github.com/dianper/moskall-app-chat" target="_blank" rel="noopener noreferrer">Diego Pereira</a>
+            Developed by <a href="https://github.com/dianper/chat-app" target="_blank" rel="noopener noreferrer">Diego Pereira</a>
         </footer>
     )
 };
